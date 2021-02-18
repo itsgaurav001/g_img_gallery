127f894c1ab50fabf25c0c36f7668b6bdec21b13
Gaurav Madaan <gmadaan91@gmail.com>
2021-02-18 11:38:55 +0530

    Plugin working started

--- src/functions.js
+++ src/functions.js
@@ -1,38 +1,5 @@
 "use strict";
 
-class gFlexGalleryCustom {
-    constructor(id) {
-        this.id = id;
-        let $el = document.getElementById(id);
-        let elements = $el.children;
-        $el.initiateGallery = function() {
-            $el.classList.add('inititatedGallery');
-            elements.forEach(function(key, elem) {
-                let $this = elem;
-                $this.classList.add('inititatedGalleryItem');
-                let poll = setInterval(function() {
-                    let imgEle = $this.querySelector('img[data-targetimg]');
-                    if (!!imgEle && imgEle.complete) {
-                        let minHeight = document.body.getAttribute('data-mhnew');
-                        let w = imgEle.naturalWidth;
-                        let h = imgEle.naturalHeight;
-                        if (w) {
-                            clearInterval(poll);
-                            $this.style.width = `minHeight * ${(w / h)}`;
-                            $this.style.webkitFlex = `${(w / h)} 1 auto`;
-                            $this.style.msFlex = `${(w / h)} 1 auto`;
-                            $this.style.flex = `${(w / h)} 1 auto`;
-                            $this.style.opacity = "1";
-                            $this.style.visibility = "visible";
-                        }
-                    }
-                }, 10);
-            });
-        };
-    }
-}
-export default gFlexGalleryCustom;
-
 function debounce(func, wait, immediate) {
     var timeout;
     return function() {
@@ -55,3 +22,37 @@ function addListenerMulti(el, s, fn) {
     s.split(' ').forEach(e => el.addEventListener(e, fn, false));
 }
 window.addListenerMulti = addListenerMulti;
+
+class gFlexGalleryCustom {
+    constructor(id) {
+        this.id = id;
+        this.initiateGallery = function() {
+            let $el = document.getElementById(id);
+            var elements = $el.querySelectorAll('.customGalleryItem');
+            $el.classList.add('inititatedGallery');
+            elements.forEach(function(elem) {
+                var $this = elem;
+                $this.classList.add('inititatedGalleryItem');
+                let poll = setInterval(function() {
+                    let imgEle = $this.querySelector('img[data-targetimg]');
+                    if (!!imgEle && imgEle.complete) {
+                        let minHeight = document.body.getAttribute('data-mhnew');
+                        let w = imgEle.naturalWidth;
+                        let h = imgEle.naturalHeight;
+                        if (w) {
+                            clearInterval(poll);
+                            $this.style.width = `minHeight * ${(w / h)}`;
+                            $this.style.webkitFlex = `${(w / h)} 1 auto`;
+                            $this.style.msFlex = `${(w / h)} 1 auto`;
+                            $this.style.flex = `${(w / h)} 1 auto`;
+                            $this.style.opacity = "1";
+                            $this.style.visibility = "visible";
+                        }
+                    }
+                }, 10);
+            });
+        }()
+    }
+}
+
+export default gFlexGalleryCustom;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import "./base.scss";
 import styles from "./styles.scss";
+
 import gFlexGalleryCustom from "./functions.js";
 
-addListenerMulti(window, 'load resize', function() {
-	const galleryTestWrap = new gFlexGalleryCustom('galleryTestWrap');
-});
+new gFlexGalleryCustom('galleryTestWrap');
